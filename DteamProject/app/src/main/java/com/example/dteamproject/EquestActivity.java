573f3cb3e45a4b6db02a51d0fb890d03274e8e09
302-14\hanul <witlim1225@gmail.com>
2021-01-08 10:31:08 +0900

    app_android 깡통프로그램

---
+++ DteamProject/app/src/main/java/com/example/dteamproject/EquestActivity.java
@@ -0,0 +1,14 @@
+package com.example.dteamproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EquestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_equest);
+    }
+}
---
+++ DteamProject/app/src/main/java/com/example/dteamproject/GtalkActivity2.java
@@ -0,0 +1,14 @@
+package com.example.dteamproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GtalkActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gtalk);
+    }
+}
