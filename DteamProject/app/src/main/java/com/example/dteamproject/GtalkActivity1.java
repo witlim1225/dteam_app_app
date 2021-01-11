2b2102aad0d26f8db5e896e4b1b0aa822708e9b4
302-08\hanul <vvtkddydvv1@naver.com>
2021-01-11 19:03:11 +0900

    Revert "임시 1 올려봄"
    This reverts commit 2df026a427a3b3e7c98856ec6c11c8d52408306b.

--- DteamProject/app/src/main/java/com/example/dteamproject/GtalkActivity1.java
+++ DteamProject/app/src/main/java/com/example/dteamproject/GtalkActivity1.java
@@ -1,68 +1,14 @@
 package com.example.dteamproject;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class GtalkActivity1 extends AppCompatActivity {
-
-    EditText etText;
-    Button btnSend;
-    private RecyclerView recyclerView;
-    MyAdapter mAdapter;
-    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gtalk1);
-<<<<<<< Updated upstream
-=======
-
-        btnSend = (Button)findViewById(R.id.btnSend);
-        etText = (EditText)findViewById(R.id.etText);
-
-        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        recyclerView.setHasFixedSize(true);
-
-        // use a linear layout manager
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
-        // specify an adapter (see also next example)
-        String[] myDataset = {"test1", "test2", "test3","test4"};
-        mAdapter = new MyAdapter(myDataset);
-        recyclerView.setAdapter(mAdapter);
-
-        btnSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //실제로 firebase에 올라가는 text
-                String stText = etText.getText().toString();
-                Toast.makeText(GtalkActivity1.this, "MSG", Toast.LENGTH_LONG).show();
-
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("message");
-
-                myRef.setValue("Hello, World!");
-            }
-        });
-
-
-
-
->>>>>>> Stashed changes
     }
 }
