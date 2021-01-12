897341b70945d42d59784c35899c28f80fc4e5ad
witlim <witlim1225@gmail.com>
2021-01-12 22:41:16 +0900

    메인페이지 구글 지도 및 마커표시(내위치 찾는게 아직 안됨. 처음 세팅으로만 지도 위치 찾았는데, 정확한 위치 x)

--- DteamProject/app/src/main/java/com/example/dteamproject/MainActivity.java
+++ DteamProject/app/src/main/java/com/example/dteamproject/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.dteamproject;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +18,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
@@ -21,10 +31,17 @@ import com.google.android.material.snackbar.Snackbar;
 //loginDTO가져온다.
 import static com.example.dteamproject.LoginActivity.loginDTO;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private GoogleMap map;
+    SupportMapFragment mapFragment;
+
+    //버튼 설정
+    Button btnMyLoc, btnSetLoc, btnMenu;
+    Button btnAddPerson1, btnAddPerson2, btnAddPerson3;
     Toolbar toolbar;
     Button btnGroupTalk, btnAddPerson;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +67,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+                //한울 위도 경도 35.153425, 126.887902 서구청 35.151932, 126.890090
+                LatLng latlng = new LatLng(35.153425, 126.887902);
+                map.moveCamera(CameraUpdateFactory.newLatLng(latlng));
+                map.moveCamera(CameraUpdateFactory.zoomTo(15));
+
+                //Marker사용
+                MarkerOptions markerOptions = new MarkerOptions().position(latlng).title("서구청");
+                map.addMarker(markerOptions);
+
+                //권한이 허용되어야 사용할 수 있도록 한다.-->현재 이게 안뜸.
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    map.setMyLocationEnabled(true);
+                }
+            }
+        });
+
+
         //네비게이션 바
        /* toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -68,7 +107,6 @@ public class MainActivity extends AppCompatActivity {
         /*navigationView.setNavigationItemSelectedListener(this);//onNavigationItemSelected()로 가게 만듬.*/
 
 
-        
         //헤드드로어에 로그인정보 표시하기
         int userlevel = 1; // 0:일반유저, 1:관리자
        /* View headerView = navigationView.getHeaderView(0);
@@ -77,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
         //Glide를 이용해서 imageview를 동그랗게(circleCrop()) 만듬.
         Glide.with(this).load(R.drawable.ic_launcher_background).circleCrop().into(imageView);*/
 
-        if(userlevel == 1){
+        if (userlevel == 1) {
             //navigationView.getMenu().findItem(R.id.communi).setVisible(true);
         }
 
@@ -96,7 +134,6 @@ public class MainActivity extends AppCompatActivity {
 
         TextView navLoginStr = headerView.findViewById(R.id.loginStr);
         navLoginStr.setText(loginDTO.getEmail());*/
-
 
 
     }
@@ -128,7 +165,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }*/
 
-    public void onFragmentSelected(int position, Bundle bundle){
+    public void onFragmentSelected(int position, Bundle bundle) {
         /*Fragment curFragment = null;
         if(position == 0){
             curFragment = fragment1;
@@ -162,5 +199,15 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
         }*/
     }
-        
+
+    //구글 맵 사용(implements OnMapReadyCallBack)으로 하는것이다.
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
 }
